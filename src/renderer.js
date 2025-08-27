0ad5430d7bbcb5c468bd98380587686a290575a2
ehsanrafi <ehsanrafianjum@gmail.com>
2025-08-27 12:02:59 +0200

    few modifications made

--- src/renderer.js
+++ src/renderer.js
@@ -1,7 +1,28 @@
 const board = document.getElementById("board");
+const goFirst = document.getElementById("go-first");
+const goSecond = document.getElementById("go-second");
+
 let currentPlayer = "X";
+let humanPlayer = "X"; // By default
+let aiPlayer = "O";
+let isHumanTurn = true;
 let cells = Array(9).fill(null);
-    
+   
+
+goFirst.addEventListener("click", () => startGame(true));
+goSecond.addEventListener("click", () => startGame(false));
+
+function startGame(humanTurn) {
+    if (!humanTurn) {
+        humanPlayer = "O";
+        aiPlayer = "X";
+        isHumanTurn = false;
+    }
+    else {
+        isHumanTurn = true;
+    }
+}
+
 function renderBoard() {
     board.innerHTML = "";
     cells.forEach((cell, i) => {
@@ -17,7 +38,17 @@ function makeMove(i) {
         cells[i] = currentPlayer;
         currentPlayer = currentPlayer == "X" ? "O" : "X";
         renderBoard();
+
+        // if (!gameOver) {
+        //     setTimeout(() => makeAIMove(), 1000);
+        // }
     }
 }
 
 renderBoard();
+
+// function makeAIMove() {
+//     const AIMove = getBestMove(cells);
+//     // cells[AIMove] = AIColor;
+//     renderBoard();
+// }
